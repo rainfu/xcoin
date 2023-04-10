66142de1bf88eedda9e2b7e96f9ab319b6ffd9d7
rainfu <rainfu@users.noreply.github.com>
2023-04-10 02:00:13 +0000

    deploy: e12b23190fced8a340c6cb97258ab8f6bc8a90c8

---
+++ assets/zh_start.md.13c1b00d.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,N as e}from"./chunks/framework.9a843496.js";const D=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"zh/start.md","lastUpdated":1681091937000}'),l={name:"zh/start.md"},o=e("",33),p=[o];function t(c,r,i,C,d,y){return n(),a("div",null,p)}const A=s(l,[["render",t]]);export{D as __pageData,A as default};
